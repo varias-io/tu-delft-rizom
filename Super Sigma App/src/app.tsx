cc2156b03560c3562b558677b2fcc1676b08d6e5
Steve Váradi <varadiista.1@gmail.com>
2023-05-12 13:56:42 +0200

    setup different environments for dev and prod

--- Super Sigma App/src/app.tsx
+++ Super Sigma App/src/app.tsx
@@ -1,9 +1,6 @@
 import pkg from '@slack/bolt';
 import { Divider, Header, Home, JSXSlack, Section, Select, Option, Mrkdwn,  } from 'jsx-slack';
 import { CustomActions } from './components/CustomActions.js';
-import { entityManager } from './utils.js';
-import { User } from './entity/User.js';
-import { Channel } from './entity/Channel.js';
 const { App } = pkg;
 
 // Initializes your app with your bot token and signing secret
@@ -18,33 +15,6 @@ app.message('hello', async ({ message, say }) => {
     await say(`hello there <@${message.user}>!`);
   }
 });
-
-/* eslint-disable */
-//@ts-ignore
-const exampleUsersAndChannels = async () => {
-  const user = entityManager.create(User, {
-    slackId: "U0550F2EP35",
-  })
-  
-  const channel1 = entityManager.create(Channel, {
-    slackId: "U0550F2EP36",
-    managers: []
-  })
-  
-  const channel2 = entityManager.create(Channel, {
-    slackId: "U0550F2EP37",
-    managers: []
-  })
-  
-  const [newChannel1, newChannel2, newUser] = await Promise.all([channel1.save(), channel2.save(), user.save()])
-  
-  console.log(newUser)
-  
-  newUser.managedChannels = [newChannel1, newChannel2]
-  
-  newUser.save()
-}
-/* eslint-enable */
 
 // Start your app
 await app.start(process.env.PORT || 9000);
