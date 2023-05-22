6d4e334a01f18f97e250fbb4bb184e52b091bd12
Ilse Huisman <I.N.Huisman@student.tudelft.nl>
2023-05-22 16:07:11 +0200

    Generate and show graph

--- Super Sigma App/src/pages/HomePage.tsx
+++ Super Sigma App/src/pages/HomePage.tsx
@@ -12,6 +12,42 @@ interface HomeProps {
     selectedChannel?: string
 }
 
+import CanvasRenderService from 'chartjs-node-canvas';
+
+const width = 1000;   // define width and height of canvas 
+const height = 1000;   
+const canvasRenderService = new CanvasRenderService.ChartJSNodeCanvas({
+    width,
+    height
+});
+
+
+const createImage = async () => {
+  const dataUrl = await canvasRenderService.renderToDataURL({
+    type: "bar",
+    data: {
+        labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4'],
+        datasets: [
+            {
+                label: 'Retweets',
+                data: [12, 5, 40, 5],
+            },
+            {
+                label: 'Likes',
+                data: [80, 42, 215, 30],
+            },
+        ],
+    },
+  }); // converts chart to image
+  return dataUrl;
+}
+
+const chart = createImage() ?? "";
+
+import {writeFile} from "node:fs/promises"
+
+writeFile("./src/assets/out.png", (await chart).replace(/^data:image\/png;base64,/, ""), "base64")
+
 export const HomePage = async ({userId, token, selectedChannel}: HomeProps) => (
     <Home>
         <Header>Welcome back to my home! :house_with_garden:</Header>
@@ -34,6 +70,7 @@ export const HomePage = async ({userId, token, selectedChannel}: HomeProps) => (
         return a.channelName.localeCompare(b.channelName);
         })}/>
         <Image src="https://ilse.rizom.test.varias.dev/table.jpeg" alt="TABLE" />
-    </Home>
+        <Image src="https://ilse.rizom.test.varias.dev/out.png" alt="chart" />  
+        </Home>
 )
 
