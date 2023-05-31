a8c3cffb2ad1917aaea19045108546ab70811759
ppvarga <VargaPalPatrik@student.tudelft.nl>
2023-05-31 15:34:35 +0200

    At least one channel must be selected

--- Super Sigma App/src/events/createSurveyAction.tsx
+++ Super Sigma App/src/events/createSurveyAction.tsx
@@ -2,6 +2,8 @@ import { JSX } from "jsx-slack/jsx-runtime"
 import { Survey } from "../entity/Survey.js"
 import { app, entityManager, findUserBySlackId, getChannelsFromSlackIds, getUsersFromChannels } from "../utils/index.js"
 import { updateHome } from "./homeOpenedAction.js"
+import  { JSXSlack, Mrkdwn, Section } from "jsx-slack"
+import { Block } from "@slack/bolt"
 
 const selectionBlockNotFound = (): object => {
   console.log("Selection block not found")
@@ -11,14 +13,27 @@ const selectionBlockNotFound = (): object => {
 /**
  * TODO: Action that happens when you click the button for creating a survey
  */
-app.action("createSurvey", async ({ ack, body, context }) => {
-    await ack()
+app.action("createSurvey", async ({ ack, body, context, client }) => {
     if(body.type != "block_actions"){
         console.log(`Unexpected body type: ${body.type}}`)
         return;
     }
     
     const selectedChannelSlackIds = Object.entries(body.view?.state.values.channelsSelect ?? selectionBlockNotFound())[0][1].selected_options.map((option: JSX.IntrinsicElements["option"]) => option.value)
+    if(!selectedChannelSlackIds.length) {
+      client.views.update({
+        token: context.botToken ?? "",
+        view_id: body.view?.id ?? "",
+        view: {
+          type: "home",
+          blocks: [noChannelSelectedErrorBlock(), ...(body?.view?.blocks ?? [])]
+        }
+      })
+      await ack();
+      return;
+    }
+    await ack()
+
     const channels = await getChannelsFromSlackIds(selectedChannelSlackIds)
     const manager = await findUserBySlackId(body.user.id)
     const participants = await getUsersFromChannels({channels: selectedChannelSlackIds, token: context.botToken ?? ""})
@@ -31,3 +46,12 @@ app.action("createSurvey", async ({ ack, body, context }) => {
 
     updateHome(body.user.id, context)
   })
+
+  const noChannelSelectedErrorBlock = (): Block => (
+    JSXSlack(
+    <Section>
+      <Mrkdwn>
+        :warning: Please select at least one channel to continue!
+      </Mrkdwn>
+    </Section>)
+  )
