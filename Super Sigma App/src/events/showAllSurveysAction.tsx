9bbbf56a0296f2bef1c9f2428d7d78efdf18ec78
Chaan van den Oudenhoven <C.vandenOudenhoven@student.tudelft.nl>
2023-06-08 11:09:26 +0200

    used name methods for api calls

--- Super Sigma App/src/events/showAllSurveysAction.tsx
+++ Super Sigma App/src/events/showAllSurveysAction.tsx
@@ -2,8 +2,15 @@ import { app } from "../utils/appSetup.js"
 import { Survey } from "../entities/Survey.js";
 import { entityManager } from "../utils/database.js";
 import { showAllSurveys } from "../pages/ShowAllSurveys.js";
+import { ActionCallback } from "../utils/index.js";
+import { EntityManager } from "typeorm";
 
-app.action("show_all_surveys", async ({ ack, client, context, body, action}) => {
+app.action("show_all_surveys", async (params) => {
+    showAllSurveysAction(params, entityManager)
+})
+
+
+export const showAllSurveysAction: ActionCallback = async ({ ack, client, context, body, action}, entityManager) => {
     await ack();
     if(action.type != "button"){
         console.error("action type is not button")
@@ -15,12 +22,12 @@ app.action("show_all_surveys", async ({ ack, client, context, body, action}) => 
     }
     const surveysIds = JSON.parse(action.value) as Survey["id"][];
 
-    const surveys = await getSurveys(surveysIds);
+    const surveys = await getSurveys(surveysIds, entityManager);
 
     await showAllSurveys(client, context.botToken ?? "", body.trigger_id ?? "", surveys, body.user.id);
-})
+}
 
-const getSurveys = async (surveysIds: string[]): Promise<Survey[]> => {
+const getSurveys = async (surveysIds: string[], entityManager: EntityManager): Promise<Survey[]> => {
    return await entityManager.createQueryBuilder(Survey, "survey")
         .leftJoinAndSelect("survey.channel", "channel")
         .leftJoinAndSelect("survey.participants", "user")
