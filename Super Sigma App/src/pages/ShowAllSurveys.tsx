ca215fb4488b556dea1f230ef5bc3e4091a93f26
Ilse Huisman <I.N.Huisman@student.tudelft.nl>
2023-06-15 11:08:11 +0200

    extract participationWarning

--- Super Sigma App/src/pages/ShowAllSurveys.tsx
+++ Super Sigma App/src/pages/ShowAllSurveys.tsx
@@ -11,7 +11,7 @@ export const showAllSurveys = async (client: AllMiddlewareArgs["client"], token:
     await client.views.open({
       token: token,
       trigger_id: trigger_id,
-      view: JSXSlack(<Modal title="Survey History"><Section><Mrkdwn>There are no finished surveys to show in the history.</Mrkdwn></Section><Divider /><Section><Mrkdwn>Did you expect to see a survey, but it did not show up? It might not have hit the threshold of 80% participation.</Mrkdwn></Section></Modal>)
+      view: JSXSlack(<Modal title="Survey History"><Section><Mrkdwn>There are no finished surveys to show in the history.</Mrkdwn></Section>{participationWarning}</Modal>)
     });
     return;
   }
@@ -72,7 +72,8 @@ export const AllSurveysBlock = async(surveys: Survey[], token: string, userSlack
   return <Modal title="Survey History" callbackId='line_graph_modal' notifyOnClose privateMetadata={JSON.stringify({filename: `${lineGraph}.png`})}>
     <Image src={`${process.env.ENDPOINT}${lineGraph}.png`} alt="Line graph visualizing the history of TMS scores for a channel." />
     {await SurveyDisplay({surveys, token, userSlackId, displayedInModal: true})}
-    <Divider />
-    <Section><Mrkdwn>Did you expect to see a survey, but it did not show up? It might not have hit the threshold of 80% participation.</Mrkdwn></Section>
+    {participationWarning}
     </Modal>
 }
+
+const participationWarning = <><Divider /><Section><Mrkdwn>Did you expect to see a survey, but it did not show up? It might not have hit the threshold of 80% participation.</Mrkdwn></Section></>
