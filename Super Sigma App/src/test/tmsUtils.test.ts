09eeb79205ac417f6051907c79235b41d11bdd4a
ppvarga <VargaPalPatrik@student.tudelft.nl>
2023-06-20 17:34:20 +0200

    test tmstopercentage

---
+++ Super Sigma App/src/test/tmsUtils.test.ts
@@ -0,0 +1,35 @@
+import { test } from "node:test"
+import assert from "assert"
+import { TMStoPercentage } from "../utils/index.js"
+
+test("TMStoPercentage - too small", async () => {
+  assert.throws(() => {
+    TMStoPercentage(0)
+  })
+})
+
+test("TMStoPercentage - too large", async () => {
+  assert.throws(() => {
+    TMStoPercentage(6)
+  })
+})
+
+test("TMStoPercentage - 1", async () => {
+  const result = TMStoPercentage(1)
+  assert.equal(result, 0)
+})
+
+test("TMStoPercentage - 2", async () => {
+  const result = TMStoPercentage(2)
+  assert.equal(result, 25)
+})
+
+test("TMStoPercentage - 3.5", async () => {
+  const result = TMStoPercentage(3.5)
+  assert.equal(result, 62.5)
+})
+
+test("TMStoPercentage - 5", async () => {
+  const result = TMStoPercentage(5)
+  assert.equal(result, 100)
+})
